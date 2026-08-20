3523cc050bb7913942ee7586c7b2520f42939978
225090082-SQA <selmaalukolo28@gmail.com>
2026-08-20 22:47:31 +0200

    Lab 1 submission

---
+++ PAP_LAB1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+    char municipalityName[50];
+    char mayorName[50];
+    int population;
+printf("\n----------------------------------------------------\n");
+    printf("Municipal Financial Management System\n");
+    printf("Welcome to Windhoek Municipality\n");
+printf("\n-----------------------------------------------------\n");
+
+printf(" Enter Municipality name :\n");
+printf("Enter the Mayor's name:\n");
+printf("Enter the population:\n");
+
+scanf("%c", &municipalityName);
+scanf("%c",&mayorName);
+scanf("%d",&population);
+
+return 0;
+}
