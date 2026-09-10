86a0b7eca5abf9f62309bf99146d31e8c4458b95
225090082-SQA <selmaalukolo28@gmail.com>
2026-09-10 21:14:23 +0200

    week 2 submission

---
+++ PAP_week2.c
@@ -0,0 +1,11 @@
+ #inlcude <stdio.h>
+ int main() {
+//question 1
+
+int number;
+FOR(i = 0 ; i < 4 ; i++) {
+    printf(" This is teh average : \n", number);
+
+}
+return 0;
+ }
---
+++ test.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Hello, Selma!\n");
+    return 0;
+
+}
+
